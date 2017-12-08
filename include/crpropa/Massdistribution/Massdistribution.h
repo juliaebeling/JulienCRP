efb481b93c7cdb7ad61b6f633ab6e54225650012
Julien Dörner <jdo@tp4.rub.de>
2017-12-08 13:21:39 +0100

    Massdistribution as superposition of HI, HII and H2

--- include/crpropa/Massdistribution/Massdistribution.h
+++ include/crpropa/Massdistribution/Massdistribution.h
@@ -5,27 +5,25 @@
 #include "crpropa/Units.h"
 #include "crpropa/Vector3.h"
 #include "crpropa/Massdistribution/Density.h"
-#include "crpropa/Massdistribution/Nakanshi2003.h"
-#include "crpropa/Massdistribution/Nakanshi2006.h"
 
+#include <string>
+#include <vector>
 
 
 namespace crpropa {
 
-class Masssdistribution: public Density {
-	std::vector<ref_ptr<Density> >Distributions;
-	int HIFlag;		// 0: none	1: Nakanshi03	2:Ferrie07
-	int HIIFlag;		// 0: none	1: Cordes91	2:Ferrie07	
-	int H2Flag;		// 0: none	1: Nakanshi06	2:Ferrie07	3:Pohl08
-	
+class Massdistribution: public Density {
+	std::vector<ref_ptr<Density> >distributionList;		// Vector with information on distribuition for HI (0); HII (1); H2 (2)
+bool isforHI=false;
+bool isforHII=false;
+bool isforH2=false;
+
 public:
-	Massdistribution();
-	Massdistribution(int HI, int HII, int H2);
-	double getDensity(const vector3d &position) const;
-	void setHI(int HI);
-	void setHII(int HII);
-	void setH2(int H2);
-	void setAll(int HI, int HII, int H2);
+	double getDensity(const Vector3d &position) const;
+	void add(Density &density);	
+	std::string getDiscription();
+
+};
 	
 } //namespace crpropa
 
--- src/Massdistribution/Massdistribution.cpp
+++ src/Massdistribution/Massdistribution.cpp
@@ -3,72 +3,49 @@
 
 namespace crpropa {
 	
-Massdistribution::Massdistribution() {
-	setAll(0,0,0);
-}
 
-Massdistribution::Massdistribution(int HI, int HII, int H2){
-	setAll(HI,HII,H2);
-}
-
-double Massdistribution::getDensity(const vector3d &position) const{
+double Massdistribution::getDensity(const Vector3d &position) const{
 	
-	double n
-	for(int i=0; i < Distributions.size(); i++)
-		n += Distritbutions[i]->getDensity(position);
-	return b;
-}
-
-void Massdistribution::setHI(int HI){
-
-	this->HI = HI;
-	switch(HI){
-	case 0:
-		Distributions[0] = new Destiny();
-	break;
-	case 1: Distributions[0] = new Nakanshi03(); 
-	break;
-	default: 
-		throw std::runtime_error("Massdistribution: unexpected Distribution for HI");
+	double n=0;
+	if(isforHI){
+		n += distributionList[0]->getHIDensity(position);
 	}
-	return;
-}
-
-void Massdistribution::setHII(int HII){
-
-	this->HII = HII;
-	switch(HII){
-	case 0:
-		Distribution[1] = new Density();
-	break;
-	case 1:
-		Distribution[1] = new Nakanshi06();
-	default:
-		throw std::runtime_error("Massdistribution: unexpected Distribution for HII");
+	if(isforHII){
+		n += distributionList[1]->getHIIDensity(position);
 	}
-	return;
-}
-
-void Massdistribution::setH2(int H2){
-
-	this->H2 = H2;
-	switch(H2){
-	case 0:
-		Distribution[2] = new Density();
-	break;
-	default: 
-		throw std::runtime_error("Massdistribution: unexpected Distribution for H2");
+	if(isforH2){
+		n += distributionList[2]->getH2Density(position);
 	}
-	return;
+	return n;
 }
 
-void Massdistribution::setAll(int HI, int HII, int H2){
+void Massdistribution::add(Density &density) { //erase actual distribution and adds new 	
 	
-	setHI(HI);
-	setHII(HII);
-	setH2(H2);
+	bool HI = density.getisforHI();
+	bool HII = density.getisforHII();
+	bool H2 = density.getisforH2();
+	auto it = distributionList.begin();
+	if(HI){
+		
+		distributionList.erase(it);	
+		distributionList.insert(it, &density);
+		isforHI = HI;
+	}
+	if(density.getisforHII()){
+		distributionList.erase(it+1);
+		distributionList.insert(it+1, &density);
+		isforHII = HII;
+	}
+	if(density.getisforH2()){
+		distributionList.erase(it+2);
+		distributionList.insert(it+2, &density);
+		isforH2 = H2;
+	}
 	return;
 }
+
+
+	
 
 } //namespace crpropa
 
