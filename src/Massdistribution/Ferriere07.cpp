7d207a553106b74a0e9daf3f61aa0c347852c280
Julien Dörner <jdo@tp4.rub.de>
2018-07-19 14:05:09 +0200

    TEST for Ferriere2007 and bugfix in model

--- src/Massdistribution/Ferriere07.cpp
+++ src/Massdistribution/Ferriere07.cpp
@@ -6,12 +6,14 @@ namespace crpropa {
 Vector3d Ferriere::CMZTrafo(const Vector3d &position) const {
 	
 	double xC = -50*pc;		//offset
-	double yC = 50*pc;
-	double ThettaC = 70/180*M_PI; //radiant
+	double yC = 50*pc;	
+	double sinTc = sin(70./180.*M_PI);	//Tc = 70 deg
+	double cosTc = cos(70./180.*M_PI);
+	
 	
 	Vector3d pos;
-	pos.x = (position.x - xC)*cos(ThettaC) + (position.y -yC)*sin(ThettaC);
-	pos.y = -(position.x -xC)*sin(ThettaC) + (position.y - yC)*cos(ThettaC);
+	pos.x = (position.x - xC)*cosTc + (position.y -yC)*sinTc;
+	pos.y = -(position.x -xC)*sinTc + (position.y - yC)*cosTc;
 	pos.z = position.z;
 	
 	// check if something went wrong with transformation
@@ -29,9 +31,15 @@ Vector3d Ferriere::CMZTrafo(const Vector3d &position) const {
 
 Vector3d Ferriere::DISKTrafo(const Vector3d &position) const { 
 
-	double alphaD = 13.5/180*M_PI;
-	double betaD = 20/180*M_PI;
-	double TettaD = 48.5/180*M_PI;
+	double alphaD = 13.5/180.*M_PI;
+	double sinAd = sin(alphaD);
+	double cosAd = cos(alphaD);
+	double betaD = 20./180.*M_PI;
+	double sinBd = sin(betaD);
+	double cosBd = cos(betaD);
+	double TettaD = 48.5/180.*M_PI;
+	double sinTd = sin(TettaD);
+	double cosTd = cos(TettaD);
 	
 	Vector3d pos;
 	
@@ -39,15 +47,15 @@ Vector3d Ferriere::DISKTrafo(const Vector3d &position) const {
 	double y = position.y;
 	double z = position.z;
 	
-	pos.x = x*cos(betaD)*cos(TettaD) - y*(sin(alphaD)*sin(betaD)*cos(TettaD) -cos(alphaD)*sin(TettaD))-z*(cos(alphaD)*sin(betaD)*cos(TettaD) +sin(alphaD)*sin(TettaD));
+	pos.x = x*cosBd*cosTd - y*(sinAd*sinBd*cosTd -cosAd*sinTd)-z*(cosAd*sinBd*cosTd +sinAd*sinTd);
 	
-	pos.y =  -x*cos(betaD)*sin(TettaD);
-	pos.y += y*(sin(alphaD)*sin(betaD)*sin(TettaD) +cos(alphaD)*cos(TettaD));
-	pos.y += z*(cos(alphaD)*sin(betaD)*sin(TettaD) -sin(alphaD)*sin(TettaD));
+	pos.y =  -x*cosBd*sinTd;
+	pos.y += y*(sinAd*sinBd*sinTd +cosAd*cosTd);
+	pos.y += z*(cosAd*sinBd*sinTd -sinAd*cosTd);
 	
-	pos.z = x*sin(betaD);
-	pos.z += y*sin(alphaD)*cos(betaD);
-	pos.z += z*cos(alphaD)*cos(betaD);
+	pos.z = x*sinBd;
+	pos.z += y*sinAd*cosBd;
+	pos.z += z*cosAd*cosBd;
 	
 	// check if something went wrong with transformation
 	bool NaN = std::isnan(pos.getR());
@@ -83,7 +91,7 @@ double Ferriere::getHIDensity(const Vector3d &position) const {
 		double z = pos.z/pc;
 		
 		double A = sqrt(pow(x,2)+pow(2.5*y,2));
-		nCMZ = 8.8*exp(-pow((A-125)/137,4))*exp(-pow(z/54,2));
+		nCMZ = 8.8*exp(-pow((A-125.)/137,4))*exp(-pow(z/54.,2));
 		
 		double nDisk = 0;		//Disk
 		
@@ -93,10 +101,12 @@ double Ferriere::getHIDensity(const Vector3d &position) const {
 		z = pos.z/pc;
 		
 		A = sqrt(pow(x,2)+pow(3.1*y,2));
-		nDisk = 0.34*exp(-pow((A-1200)/438,4))*exp(-pow(z/120,2));
+		nDisk = 0.34*exp(-pow((A-1200.)/438.,4))*exp(-pow(z/120,2));
 		
 		
 		n = nCMZ + nDisk;
+		
+		
 		
 	}
 	else{ //AUßEN
@@ -115,7 +125,7 @@ double Ferriere::getHIDensity(const Vector3d &position) const {
 		nCold += 0.859*exp(-pow(z/(127*a),2));
 		nCold += 0.047*exp(-pow(z/(318*a),2));
 		nCold += 0.094*exp(-fabs(z)/(403*a));
-		nCold *= 0.304/(pow(a,2));
+		nCold *= 0.340/(pow(a,2));
 		
 		
 		double nWarm =0;		//warm HI
@@ -124,8 +134,8 @@ double Ferriere::getHIDensity(const Vector3d &position) const {
 		nWarm += (0.283 - 0.142/a)*exp(-fabs(z)/(403*a));
 		nWarm *= 0.226/a;
 		
-		
-		n = nWarm + nCold;
+		n=nWarm+nCold;
+
 	}
 	
 	// check if density is NAN
@@ -159,42 +169,52 @@ double Ferriere::getHIIDensity(const Vector3d &position) const {
 	double y = position.y/pc;
 	double z = position.z/pc;
 	
-	double nWIM =0;		//warm IM
-	double nVHIM = 0;	//very hot IM
 	
-	nWIM += exp(-(pow(x,2)+pow(y+10,2))/pow(145,2))*exp(-pow((z+20)/26,2));
-	nWIM += 0.009*exp(-pow((R/pc-3700)/(0.5*3750),2))*1/pow(cosh(z/140),2);
-	nWIM += 0.005*cos(M_PI*R/pc*0.5/17000)*1/pow(cosh(z/950),2);
 	
-	nWIM *= 8.0;	//center Density for scaling 
 	
-	double alphaVH = 21/180*M_PI;		//angel for very hot IM in radiant 
-	double etta = y*cos(alphaVH)+z*sin(alphaVH);
-	double chi = -y*sin(alphaVH)+z*cos(alphaVH);
+	//warm interstellar matter
+	double nWIM =0;		
+		
+	nWIM += exp(-(pow(x,2)+pow(y+10,2))/pow(145,2))*exp(-pow((z+20)/26.,2));
+	nWIM += 0.009*exp(-pow((R/pc-3700)/(0.5*3700),2))*1/pow(cosh(z/140.),2);
+	nWIM += 0.005*cos(M_PI*R/pc*0.5/17000)*1/pow(cosh(z/950.),2);
+	
+	nWIM *= 8.0;	//center Density for scaling [cm^-3]
+	
+	
+	//very hot interstellar matter
+	double nVHIM = 0;
+	double alphaVH = 21./180*M_PI;		//angel for very hot IM in radiant 
+	double cosA = cos(alphaVH);
+	double sinA = sin(alphaVH);
+	double etta = y*cosA+z*sinA;		//transformation for VHIM
+	double chi = -y*sinA+z*cosA;
 	
 	nVHIM = 0.29*exp(-((pow(x,2)+pow(etta,2))/pow(162,2)+pow(chi/90,2)));
 	
 	n = nWIM + nVHIM;
+	
 	}
 	else {		//außen
 		
 		double z = position.z/pc;
 		
 		double nWarm = 0;
-		double nHot = 0;
-		
 		nWarm += 0.0237*exp(-(pow(R,2)-pow(Rsun,2))/pow(37*kpc,2))*exp(-fabs(z)/1000);
 		nWarm += 0.0013*exp(-(pow(R-4*kpc,2)-pow(Rsun-4*kpc,2))/pow(2*kpc,2))*exp(-fabs(z)/150);
 		
-		nHot += 0.12*exp(-(R-Rsun)/4.9*kpc);
-		nHot += 0.88*exp(-(pow(R-4.5*kpc,2)-pow(Rsun-4.5*kpc,2))/pow(2.9*kpc,2));
 		
+		
+		
+		double nHot = 0;
+		nHot += 0.12*exp(-(R-Rsun)/(4.9*kpc));
+		nHot += 0.88*exp(-(pow(R-4.5*kpc,2)-pow(Rsun-4.5*kpc,2))/pow(2.9*kpc,2));
 		nHot *= pow(R/Rsun, -1.65);
 		nHot *= exp(-fabs(z)/(1500*pow(R/Rsun,1.65)));
 		nHot *= 4.8e-4;
+		
 		n= nWarm + nHot;
-
-		n=nWarm;		
+		
 	}
 	
 	// check if density is NAN
@@ -234,7 +254,7 @@ double Ferriere::getH2Density(const Vector3d &position) const{
 		double z = pos.z/pc;
 		
 		double A = sqrt(pow(x,2)+pow(2.5*y,2));
-		nCMZ = exp(-pow((A-125)/137,4))*exp(-fabs(z)/18);
+		nCMZ = exp(-pow((A-125.)/137.,4))*exp(-pow(z/18.,2));
 		nCMZ *= 150;		//Density at Center for scale
 		
 		pos =DISKTrafo(position);
@@ -251,12 +271,12 @@ double Ferriere::getH2Density(const Vector3d &position) const{
 	else {		//outer region
 		double z = position.z/pc;
 		n = pow(R/Rsun, -0.58);
-		n *= exp(-(pow(R-4*kpc,2)-pow(Rsun-4*kpc,2))/pow(2.9*kpc,2));
+		n *= exp(-(pow(R-4.5*kpc,2)-pow(Rsun-4.5*kpc,2))/pow(2.9*kpc,2));
 		n *= exp(-pow(z/(81*pow(R/Rsun,0.58)),2));
-		n *= 0.58;		//density at center for scale
+		n *= 0.58;		//density at center for scale [cm^-3]
 	}
 	
-		// check if density is NAN
+	// check if density is NAN
 	// return 0 instead
 	bool NaN = std::isnan(n);
 	if(NaN == true){
--- test/testDensity.cpp
+++ test/testDensity.cpp
@@ -16,10 +16,10 @@ TEST(testConstantDensity, SimpleTest) {
 	//test constant Density in all types and in total density (output)
 	constantDensity n(2/ccm,3/ccm, 2/ccm);
 	Vector3d p(1*pc,2*pc,1*kpc); 	// random position for testing density
-	double HI = n.getHIDensity(&p);
-	double HII = n.getHIIDensity(&p);
-	double H2 = n.getH2Density(&p);
-	double Hges = n.getDensity(&p);
+	double HI = n.getHIDensity(p);
+	double HII = n.getHIIDensity(p);
+	double H2 = n.getH2Density(p);
+	double Hges = n.getDensity(p);
 	EXPECT_DOUBLE_EQ(HI, 2e6);	// density output in m^-3
 	EXPECT_DOUBLE_EQ(HII, 3e6);
 	EXPECT_DOUBLE_EQ(H2, 2e6);
@@ -40,9 +40,9 @@ TEST(testConstantDensity, SimpleTest) {
 	n.setHII(500.);
 	n.setH2(500.);
 	
-	HI = n.getHIDensity(&p);		
-	HII = n.getHIIDensity(&p);
-	H2 = n.getH2Density(&p);
+	HI = n.getHIDensity(p);		
+	HII = n.getHIIDensity(p);
+	H2 = n.getH2Density(p);
 	
 	//check if output is changed too 500 in types and is 0 (all deaktivated) for Hges
 	EXPECT_DOUBLE_EQ(HI, 500);	
@@ -64,11 +64,192 @@ TEST(testConstantDensity, SimpleTest) {
 	
 	//get type density is independent from type activation, get density is not independent 
 	//check if get density returns 0. (should give a error massage in log file)
-	Hges = n.getDensity(&p);
+	Hges = n.getDensity(p);
 	EXPECT_DOUBLE_EQ(Hges, 0);
 }
 
-
+TEST(testMassdistribution, SimpleTest) {
 	
+	Massdistribution MD;
+	MD.add(new constantDensity(1,1,1));	//only HI types
+	MD.add(new constantDensity(0,2,0));	//only HII type
+	MD.add(new constantDensity(0,0,3));	//only H2 type
+
+	Vector3d p(50*pc, 20*pc, -100*pc);	//random position for testing density 
+	
+	//check get density output
+	double HI = MD.getHIDensity(p);
+	double HII= MD.getHIIDensity(p);
+	double H2 = MD.getH2Density(p);
+	double n = MD.getDensity(p);
+	EXPECT_DOUBLE_EQ(HI,1);
+	EXPECT_DOUBLE_EQ(HII,2);
+	EXPECT_DOUBLE_EQ(H2,3);
+	EXPECT_DOUBLE_EQ(n,6);	//total density 2+2+3
+	
+	//check get function for type 
+	EXPECT_TRUE(MD.getisforHI());
+	EXPECT_TRUE(MD.getisforHII());
+	EXPECT_TRUE(MD.getisforH2());
+
+} 
+
+TEST(testMassdistributionSuperposition, SimpleTest) {
+
+	MassdistributionSuperposition MS;
+	MS.addDensity(new constantDensity(1,1,2));	//sum 4
+	MS.addDensity(new constantDensity(2,3,1));	//sum 6
+	
+	Vector3d p(50*pc,10*pc,-30*pc);	//random position for testing density
+	EXPECT_DOUBLE_EQ(MS.getHIDensity(p),3);
+	EXPECT_DOUBLE_EQ(MS.getHIIDensity(p),4);
+	EXPECT_DOUBLE_EQ(MS.getH2Density(p),3);
+	EXPECT_DOUBLE_EQ(MS.getDensity(p),10);	//sum of sums
+	
+}
+
+TEST(testCordes, SimpleTest) {
+
+	Cordes n;
+	
+	//check type Information
+	EXPECT_FALSE(n.getisforHI());
+	EXPECT_TRUE(n.getisforHII());
+	EXPECT_FALSE(n.getisforH2());
+
+	Vector3d p(3.1*kpc,2.9*kpc,-30*pc);	//position for testing density	
+	
+	double HII = n.getHIIDensity(p);
+	double Hges = n.getDensity(p);
+	
+	EXPECT_NEAR(HII, 184500.,1);	// output in m^-3 ; uncertainty of 1e-6 cm^-1 
+	EXPECT_NEAR(Hges, 184500.,1);	
+	p.z=30*pc;			// invariant density for +/- z
+	EXPECT_DOUBLE_EQ(HII,n.getDensity(p)); 
+}
+
+TEST(testNakanishi, SimpleTest) {
+
+	Nakanishi n;
+	
+	//check type Information
+	EXPECT_TRUE(n.getisforHI());
+	EXPECT_FALSE(n.getisforHII());
+	EXPECT_TRUE(n.getisforH2());
+	
+	Vector3d p(4*kpc,-2.5*kpc,-0.85*kpc);	//position for testing density
+	
+	//testing HI component
+	EXPECT_NEAR(n.getHIPlanedensity(p),162597,1);	//uncertaincy of 1e-6 cm^-3 
+	EXPECT_NEAR(n.getHIScaleheight(p),0.3109*kpc,0.1*pc); 
+	EXPECT_NEAR(n.getHIDensity(p),914,1); 	//uncertainc 1e-6 cm^-3 
+	
+	//testing HII compontent
+	EXPECT_NEAR(n.getH2Planedensity(p),741999,1); //uncertaincy of 1e-6 cm^-3
+	EXPECT_NEAR(n.getH2Scaleheight(p),88.2*pc,0.1*pc);
+	EXPECT_NEAR(n.getH2Density(p),0,1);
+	
+	//testing total Density
+	EXPECT_NEAR(n.getDensity(p),914,2); //double uncertaincy for both type á 1cm^-3
+	
+	
+	
+	p = Vector3d(50*pc,100*pc,10*pc);	// second position for testing density
+	
+	//testing HI component
+	EXPECT_NEAR(n.getHIPlanedensity(p),543249,1);
+	EXPECT_NEAR(n.getHIScaleheight(p),125.6*pc,0.1*pc);
+	EXPECT_NEAR(n.getHIDensity(p),540867,1);
+	
+	//testing H2 component
+	EXPECT_NEAR(n.getH2Planedensity(p),10556748,1);
+	EXPECT_NEAR(n.getH2Scaleheight(p),57.2*pc,0.1*pc);
+	EXPECT_NEAR(n.getH2Density(p),10335137,1);
+	
+	//test set type function
+	n.setisforHI(false);
+	EXPECT_FALSE(n.getisforHI());
+	EXPECT_TRUE(n.getisforH2());
+	
+	n.setisforH2(false);
+	EXPECT_FALSE(n.getisforHI());
+	EXPECT_FALSE(n.getisforH2());
+	
+	//check if density output is zero if all density-types are deaktivated (should give warning in log-file)
+	EXPECT_DOUBLE_EQ(n.getDensity(p),0);	
+}
+
+TEST(testFerriere, SimpleTest) {
+	
+	Ferriere n;
+	
+	//check type information
+	
+	EXPECT_TRUE(n.getisforHI());
+	EXPECT_TRUE(n.getisforHII());
+	EXPECT_TRUE(n.getisforH2());
+	
+	//testing density in inner Ring (R <= 3*kpc)
+	Vector3d p(-60*pc,60*pc,-20*pc);	//testing position in region of CMZ
+	
+	//test CMZ Trafo
+	Vector3d Trafo;
+	Trafo = n.CMZTrafo(p);
+	EXPECT_NEAR(Trafo.x,5.9767*pc,1e-4*pc);
+	EXPECT_NEAR(Trafo.y,12.8171*pc,1e-4*pc);
+	EXPECT_DOUBLE_EQ(Trafo.z,p.z);	//no transformation in z component
+	
+	//test DISK Trafo
+	Trafo = n.DISKTrafo(p);
+	EXPECT_NEAR(Trafo.x,11.0660*pc,1e-4*pc);
+	EXPECT_NEAR(Trafo.y,82.5860*pc,1e-4*pc);
+	EXPECT_NEAR(Trafo.z,-25.6338*pc,1e-4*pc);
+	
+	//testing density 
+	EXPECT_NEAR(n.getHIDensity(p),6237723,1); 	//uncertaincy 1e-6 cm^-3
+	EXPECT_NEAR(n.getH2Density(p),35484825,1); 
+	EXPECT_NEAR(n.getHIIDensity(p),5570335,1);	
+	EXPECT_NEAR(n.getDensity(p),47292883,1);
+	
+	Vector3d p2(500*pc,900*pc,35*pc);	//testing position in region of the DISK
+	EXPECT_NEAR(n.getHIIDensity(p2),48190,1);
+	EXPECT_NEAR(n.getHIDensity(p2),5,1);
+	EXPECT_NEAR(n.getH2Density(p2),0,1);
+	EXPECT_NEAR(n.getDensity(p2),48195,1);
+	
+	//testing the outer region R>3kpc
+	
+	Vector3d p3(5*kpc,4*kpc,-29*pc);	//testing position with 3kpc < R < R_sun
+	EXPECT_NEAR(n.getHIDensity(p3),540607,1);
+	EXPECT_NEAR(n.getHIIDensity(p3),66495 ,1);
+	EXPECT_NEAR(n.getH2Density(p3),2492685,1);
+	EXPECT_NEAR(n.getDensity(p3),3099787,1);
+	
+	Vector3d p4(10*kpc,2*kpc,50*pc);	//testing position with R > R_sun
+	EXPECT_NEAR(n.getHIDensity(p4),431294,1);
+	EXPECT_NEAR(n.getHIIDensity(p4),22109,1);
+	EXPECT_NEAR(n.getH2Density(p4),54099,1);
+	EXPECT_NEAR(n.getDensity(p4),507502,1);
+	
+	//test get/set type funktion
+	
+	n.setisforHI(false);
+	EXPECT_FALSE(n.getisforHI());
+	EXPECT_TRUE(n.getisforHII());
+	EXPECT_TRUE(n.getisforH2());
+	
+	n.setisforHII(false);
+	EXPECT_FALSE(n.getisforHI());
+	EXPECT_FALSE(n.getisforHII());
+	EXPECT_TRUE(n.getisforH2());
+	
+	n.setisforH2(false);
+	EXPECT_FALSE(n.getisforHI());
+	EXPECT_FALSE(n.getisforHII());
+	EXPECT_FALSE(n.getisforH2());
+	
+	//check if density is set to zero if all types are deaktivated (schould give warning in log-file)
+	EXPECT_DOUBLE_EQ(n.getDensity(p),0);
+}
 
 } //namespace crpropa
