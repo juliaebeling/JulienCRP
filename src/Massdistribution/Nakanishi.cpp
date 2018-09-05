5d8f25d4139181c2db49bd5c8e5b28a7c2c7dcbe
Julien Dörner <jdo@tp4.rub.de>
2018-09-05 14:10:24 +0200

    change unit use in density distribution Nakanishi and change in construktor

--- src/Massdistribution/Nakanishi.cpp
+++ src/Massdistribution/Nakanishi.cpp
@@ -2,61 +2,47 @@
 
 namespace crpropa{ 
 
-Nakanishi::Nakanishi(){
-	isforHI = true;
-	isforHII = false;
-	isforH2 = true;		
-}
-
 
 
 double Nakanishi::getHIScaleheight(const Vector3d &position) const{
 	
-	double x = position.x/kpc;
-	double y = position.y/kpc;
-	double R = sqrt(pow(x,2)+pow(y,2));	//Galactic Radius		
-	double scaleheight = 1.06*(116.3 +19.3*R+4.1*pow(R,2)-0.05*pow(R,3));
+	double R = sqrt(pow(x,2)+pow(y,2));	//radius in galactic plane
+	double scaleheight = 1.06*(116.3 +19.3*R/kpc+4.1*pow(R/kpc,2)-0.05*pow(R/kpc,3));
 	return scaleheight*pc;
 	}
 
 double Nakanishi::getHIPlanedensity(const Vector3d &position) const {
 	
-	double x = position.x/kpc;
-	double y = position.y/kpc;
-	double R = sqrt(pow(x,2)+pow(y,2));
-	double planedensity = 0.94*(0.6*exp(-R/2.4)+0.24*exp(-pow((R-9.5)/4.8,2)));
+	double R = sqrt(pow(x,2)+pow(y,2));	//radius in galactic plane
+	double planedensity = 0.94*(0.6*exp(-R/(2.4*kpc))+0.24*exp(-pow((R-9.5*kpc)/(4.8*kpc),2)));
 	return planedensity/ccm;
 	}
 
 
 double Nakanishi::getH2Scaleheight(const Vector3d &position) const {
 
-	double x = position.x/kpc;
-	double y = position.y/kpc;
-	double R = sqrt(pow(x,2)+ pow(y,2));
-	double Scaleheight = 1.06e-3*( 10.8*exp(0.28*R)+42.78);
-	return Scaleheight*kpc;
+
+	double R = sqrt(pow(x,2)+ pow(y,2)); //radius in galactic plane
+	double scaleheight = 1.06*( 10.8*exp(0.28*R/kpc)+42.78);
+	return scaleheight*pc;
 }
 
 double Nakanishi::getH2Planedensity(const Vector3d &position) const {
 
-	double x = position.x/kpc;
-	double y = position.y/kpc;
-	double R = sqrt(pow(x,2)+pow(y,2));
-	double Planedensity = 11.2*exp(-pow(R,2)/0.874) +0.83*exp(-pow((R-4)/3.2,2));
-	return 0.94*Planedensity/ccm;
+	double R = sqrt(pow(x,2)+pow(y,2)); //radius in galactic plane
+	double planedensity = 11.2*exp(-pow(R,2)/(0.874*kpc*kpc) +0.83*exp(-pow((R-4*kpc)/3.2*kpc,2));
+	return 0.94/ccm*planedensity;
 }
 
 double Nakanishi::getHIDensity(const Vector3d &position) const {
 	
 	double n = 0; //density
-	double z = position.z;
 	double planedensity = getHIPlanedensity(position);
 	double scaleheight = getHIScaleheight(position);
-	n= planedensity*pow(0.5,pow(z/scaleheight,2));
+	n= planedensity*pow(0.5,pow(position.z/scaleheight,2));
 	
 	// check if density is NAN
-	// return 0 instead
+	// return 0 instead and give warning
 	bool NaN = std::isnan(n);
 	if(NaN == true){
 		KISS_LOG_WARNING
@@ -74,13 +60,12 @@ double Nakanishi::getHIDensity(const Vector3d &position) const {
 double Nakanishi::getH2Density(const Vector3d &position) const {
 	
 	double n = 0; //density
-	double z = position.z;
-	double plane = getH2Planedensity(position);
+	double planedensity = getH2Planedensity(position);
 	double scaleheight = getH2Scaleheight(position);
-	n= plane*pow(0.5,pow(z/scaleheight,2));
+	n= planedensity*pow(0.5,pow(position.z/scaleheight,2));
 	
 	// check if density is NAN
-	// return 0 instead
+	// return 0 instead and give warning 
 	bool NaN = std::isnan(n);
 	if(NaN == true){
 		KISS_LOG_WARNING
@@ -122,7 +107,7 @@ double Nakanishi::getNucleonDensity(const Vector3d &position) const {
 	if(isforHI)
 		n += getHIDensity(position);
 	if(isforH2)
-		n += 2*getH2Density(position);
+		n += 2*getH2Density(position);	// weight 2 for molecular hydrogen
 	
 	//check if any density is activ and give warning if not
 	bool anyDensityActive = isforHI||isforH2;
